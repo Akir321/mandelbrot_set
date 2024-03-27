5af0672142fccc47c7813ad419d9dbd1ee70ddbb
Akir321 <anton-1945@bk.ru>
2024-03-27 23:03:28 +0300

    naive calculation
    I also was compiling without -O3 actually, so now everything's faster

--- makefile
+++ makefile
@@ -21,26 +21,40 @@ INCLUDE = $(SRC_DIR)mandelbrot.h
 
 SIMD    = $(OBJ_DIR)set_calc_simd.o
 
+NAIVE   = $(OBJ_DIR)set_calc_naive.o
+
+EXEC    = mandelbrot_naive \
+          mandelbrot_simd 
+
 		  
-all: mandelbrot_simd
+all: mandelbrot_naive
+
 
 mandelbrot_simd: $(OBJECTS) $(SIMD)
-	$(CXX) -O3   $(OBJECTS) $(SIMD) -o $@ -L/usr/lib -lSDL2
+	$(CXX)       $(OBJECTS) $(SIMD)   -o $@ -L/usr/lib -lSDL2
 
-$(OBJ_DIR)set_calc_simd.o: $(SRC_DIR)set_calc_simd.cpp $(INCLUDE)
-	$(CXX) -c $< -o $@ $(CXX_FLAGS) -mavx2 
+mandelbrot_naive: $(OBJECTS) $(NAIVE)
+	$(CXX)        $(OBJECTS) $(NAIVE) -o $@ -L/usr/lib -lSDL2
 
-$(OBJ_DIR)main.o:          $(SRC_DIR)main.cpp          $(INCLUDE)
+
+
+$(OBJ_DIR)set_calc_simd.o:  $(SRC_DIR)set_calc_simd.cpp  $(INCLUDE)
+	$(CXX) -O3 -c $< -o $@ $(CXX_FLAGS) -mavx2 
+
+$(OBJ_DIR)set_calc_naive.o: $(SRC_DIR)set_calc_naive.cpp $(INCLUDE)
+	$(CXX) -O3 -c $< -o $@ $(CXX_FLAGS) 
+
+$(OBJ_DIR)main.o:           $(SRC_DIR)main.cpp           $(INCLUDE)
 	$(CXX) -c $< -o $@ $(CXX_FLAGS)
 
-$(OBJ_DIR)work_with_SDL.o: $(SRC_DIR)work_with_SDL.cpp $(SRC_DIR)work_with_SDL.h
+$(OBJ_DIR)work_with_SDL.o:  $(SRC_DIR)work_with_SDL.cpp  $(SRC_DIR)work_with_SDL.h
 	$(CXX) -c $< -o $@ $(CXX_FLAGS)
 
-$(OBJ_DIR)mandelbrot.o:    $(SRC_DIR)mandelbrot.cpp    $(INCLUDE)
+$(OBJ_DIR)mandelbrot.o:     $(SRC_DIR)mandelbrot.cpp     $(INCLUDE)
 	$(CXX) -c $< -o $@ $(CXX_FLAGS)
 
-$(OBJ_DIR)html_logfile.o:  $(SRC_DIR)html_logfile.cpp  $(SRC_DIR)html_logfile.h
+$(OBJ_DIR)html_logfile.o:   $(SRC_DIR)html_logfile.cpp   $(SRC_DIR)html_logfile.h
 	$(CXX) -c $< -o $@ $(CXX_FLAGS)
 
 clean:
-	rm mandelbrot $(OBJ_DIR)*.o
+	rm $(EXEC) $(OBJ_DIR)*.o
---
+++ source/set_calc_naive.cpp
@@ -0,0 +1,88 @@
+#include <SDL2/SDL.h>
+#include <assert.h>
+
+#include "mandelbrot.h"
+
+#define BASELIN
+
+
+void calculateSet(SDL_Renderer *renderer, const CoordData *data);
+
+static inline int  calculatePointN(float x0, float y0);
+
+static inline void printPoint(SDL_Renderer *renderer, 
+                              int x, int y, int N);
+
+
+void calculateSet(SDL_Renderer *renderer, const CoordData *data)
+{
+    assert(renderer);
+    assert(data);
+
+    float dxZoomed = dx * data->zoomMult;
+
+    for (int curY = 0; curY < WindowHeight; curY++)
+    {
+        float x0 = ( (            - WindowWidth  / 2.f) * dx ) * data->zoomMult +
+                    CenterX + data->shiftX;
+
+        float y0 = ( ((float)curY - WindowHeight / 2.f) * dy ) * data->zoomMult +
+                    CenterY + data->shiftY;
+
+        for (int curX = 0; curX < WindowWidth; curX++, x0 += dxZoomed)
+        {
+            int N = calculatePointN(x0, y0);
+
+            printPoint(renderer, curX, curY, N);
+        }
+    }
+}
+
+static inline int calculatePointN(float x0, float y0)
+{
+    int N = 0;
+
+#ifndef BASELINE
+
+    float x = x0, 
+          y = y0;
+
+    for ( ; N < MaxPointN; N++)
+    {
+        float x2 = x * x,
+              y2 = y * y,
+              xy = x * y;
+
+        float r2 = x2 + y2;
+
+        if (r2 >= Radius2Max) return N;
+
+        x = x2 - y2 + x0;
+        y = xy + xy + y0;
+    }
+
+#endif
+
+    return N;
+}
+
+#define BYTE char
+
+static inline void printPoint(SDL_Renderer *renderer, 
+                              int x, int y, int N)
+{
+    float I = sqrtf((float)N / (float)MaxPointN) * 255.f;
+    
+    BYTE c = (BYTE) I;
+
+    if (N >= MaxPointN) 
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+
+    else                
+        SDL_SetRenderDrawColor(renderer, 0, (BYTE)(c%2 * 64), (BYTE)(c%2 * 64),
+                               SDL_ALPHA_OPAQUE);
+
+    SDL_RenderDrawPoint(renderer, x, y);
+}
+
+#undef BYTE
